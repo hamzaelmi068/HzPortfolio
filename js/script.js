0c4026bdd62a10b0fab44e2ba8d1a6e7f121cc40
Hamza <hamza.elmi@mail.utoronto.ca>
2024-02-23 19:30:21 -0500

    navbar

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 /**
- * @copyright codewithsadee 2023
- * @author Sadee <codewithsadee24@gmail.com>
+ * @copyright Hz 2024
+ * @author Sadee <hamzaelmi067@gmail.com>
  */
 
 "use strict";
+
+const header = document.querySelector("[data-header]");
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+
+navToggleBtn.addEventListener("click", function () {
+  header.classList.toggle("nav-active");
+  this.classList.toggle("active");
+});
