2e3c90806e82a574dd368edfcd896809321c6b66
Hamza <hamza.elmi@mail.utoronto.ca>
2024-02-26 21:32:51 -0500

    stage 2

--- js/script.js
+++ js/script.js
@@ -29,3 +29,14 @@ for (let i = 0; i < navBarLinks.length; i++){
 // back to top and header
 
 const backTopBtn = document.querySelector("[data-back-to-top]");
+
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
